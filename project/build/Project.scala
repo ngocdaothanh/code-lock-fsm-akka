119a181c11c917dadc211e899bb128a4b44feeac
linh <linh@linhs-iMac.local>
2010-08-22 17:00:11 +0900

    First commit

---
+++ project/build/Project.scala
@@ -0,0 +1,7 @@
+import sbt._
+
+class Project(info: ProjectInfo) extends DefaultProject(info) {
+  //val akkaHome = Path.fromFile(System.getenv("AKKA_HOME"))
+  val akkaHome = Path.fromFile(System.getProperty("user.home") + "/plain/akka")
+  override def unmanagedClasspath = (akkaHome / "dist" **"*.jar") +++ (akkaHome / "lib_managed" ** "*.jar")
+}
---
+++ src/main/scala/demo/Lock.scala
@@ -0,0 +1,41 @@
+package demo
+
+import actor.Fsm
+import se.scalablesolutions.akka.actor.Actor
+
+case class Button(digit: Char)
+
+object Lock {
+  val TIMEOUT = 5000
+}
+
+class Lock(code: String) extends Actor with Fsm[String] {
+  def initialState = State(NextState, locked, "")
+
+  def locked: StateFunction = {
+    case Event(Button(digit), sofar) =>
+      val sofar2 = sofar + digit
+      if (sofar2 == code) {
+        println("Opened")
+        State(NextState, open, "", Some(Lock.TIMEOUT))
+      } else {
+        if (sofar2.length < code.length) {
+          println("So far: " + sofar2)
+          State(NextState, locked, sofar2, Some(Lock.TIMEOUT))
+        } else {
+          println("Wrong code")
+          initialState
+        }
+      }
+
+    case Event(StateTimeout, _) =>
+      println("Reset")
+      initialState
+  }
+
+  def open: StateFunction = {
+    case Event(_, _) =>
+      println("Locked")
+      initialState
+  }
+}
