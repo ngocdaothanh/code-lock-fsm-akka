42a013e0fda6c893b1ca91c70acf3981a3912bff
Linh <gplinh@gmail.com>
2010-09-16 00:16:58 +0900

    Reformat README

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -3,5 +3,6 @@ import sbt._
 class Project(info: ProjectInfo) extends DefaultProject(info) {
   //val akkaHome = Path.fromFile(System.getenv("AKKA_HOME"))
   val akkaHome = Path.fromFile(System.getProperty("user.home") + "/plain/akka")
-  override def unmanagedClasspath = (akkaHome / "dist" **"*.jar") +++ (akkaHome / "lib_managed" ** "*.jar")
+
+  override def unmanagedClasspath = (akkaHome ** "*.jar") //+++ (akkaHome / "lib_managed" ** "*.jar")
 }
